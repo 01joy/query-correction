93cb8ef72db69b7e471b33e0af13039127d57254
czl <zhenlin.chen@qq.com>
2019-08-26 21:38:16 +0800

    修改代码使得可以连续测试

--- code/test.py
+++ code/test.py
@@ -10,11 +10,10 @@ from preprocess_sohu_news import preprocess_sohu_news
 from trans import find_words
 import copy
 
-sohu_path=r'/root/data/news_sohusite_xml.100000.dat'
+sohu_path=r'../data/news_sohusite_xml.smarty.dat'
 w2p = "../data/word2pinyin.json"
 p2w = '../data/pinyin2word.json'
-sentence = '刘得华'
-
+#sentence = '刘得华'
 
 news=preprocess_sohu_news(sohu_path)
 
@@ -28,16 +27,20 @@ for one_news in news:
 
 ng = NGram(text, 3)
 
-sentence = [i for i in sentence]
-corrections=[]
+while True:
+    sentence = input('please input your query:')
+    sentence = [i for i in sentence]
+    corrections=[]
+    
+    for i,c in enumerate(sentence):
+        sent=copy.deepcopy(sentence)
+        cands = find_words(c, w2p, p2w)
+        for cand in cands:
+            sent[i]=cand
+            corr = ''.join(sent)
+            corrections.append([corr,ng.cal_sentence_prob(corr)])
+    
+    corrections=sorted(corrections,key=lambda x:x[1],reverse=True)
+    print(corrections[0])
 
-for i,c in enumerate(sentence):
-    sent=copy.deepcopy(sentence)
-    cands = find_words(c, w2p, p2w)
-    for cand in cands:
-        sent[i]=cand
-        corr = ''.join(sent)
-        corrections.append([corr,ng.cal_sentence_prob(corr)])
-
-corrections=sorted(corrections,key=lambda x:x[1],reverse=True)
-print(corrections[0])
+    
