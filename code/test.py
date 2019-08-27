fd527961953f4592969606ab26a3aa55c51b3af5
czl <zhenlin.chen@qq.com>
2019-08-27 10:45:52 +0800

    train和test代码分开

--- code/test.py
+++ code/test.py
@@ -5,31 +5,21 @@ Created on Sun Aug 25 23:59:57 2019
 @author: Zhenlin
 """
 
-from n_gram_model import NGram
-from preprocess_sohu_news import preprocess_sohu_news
+#from n_gram_model import NGram
+#from preprocess_sohu_news import preprocess_sohu_news
 from trans import find_words
 import copy
+import pickle
 
-import readline
-
-sohu_path=r'/Users/bytedance/Downloads/news_sohusite_xml-utf8-10000.dat'
+model_path=r'n-gram.model'
 w2p = "../data/w2p_v2.json"
 p2w = '../data/p2w_v2.json'
-N = 3 # n-gram
+
 M = 5 # print list number
-#sentence = '刘得华'
 
-news=preprocess_sohu_news(sohu_path)
-
-text=[]
-for one_news in news:
-    chars=[]
-    for c in one_news:
-        if len(c.strip())>0:
-            chars.append(c.strip())
-    text.append(chars)
-
-ng = NGram(text, N)
+f = open(model_path, 'rb')
+classifier = pickle.load(f)
+f.close()
 
 while True:
     sentence = input('please input your query, q for quit:')
@@ -45,9 +35,10 @@ while True:
         for cand in cands:
             sent[i]=cand
             corr = ''.join(sent)
-            corrections.append([corr,ng.cal_sentence_prob(corr)])
+            corrections.append([corr,classifier.cal_sentence_prob(corr)])
     
     corrections=sorted(corrections,key=lambda x:x[1],reverse=True)
     for i in range(min(len(corrections), M)):
         print('%d\t%s\t%e'%(i, corrections[i][0], corrections[i][1]))
+    
     
---
+++ code/train.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 23:59:57 2019
+
+@author: Zhenlin
+"""
+
+from n_gram_model import NGram
+from preprocess_sohu_news import preprocess_sohu_news
+import pickle
+
+sohu_path=r'/Users/bytedance/Downloads/news_sohusite_xml-utf8-10000.dat'
+model_path=r'n-gram.model'
+N = 3 # n-gram
+#sentence = '刘得华'
+
+news=preprocess_sohu_news(sohu_path)
+
+text=[]
+for one_news in news:
+    chars=[]
+    for c in one_news:
+        if len(c.strip())>0:
+            chars.append(c.strip())
+    text.append(chars)
+
+ng = NGram(text, N)
+
+
+f = open(model_path, 'wb')
+pickle.dump(ng, f)
+f.close()
