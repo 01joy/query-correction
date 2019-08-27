a35eed33119ea06f928ae4c45521877e09a5f1d4
root <root@debug010000002015.local.alipay.net>
2019-08-27 09:13:11 +0800

    添加数据

--- code/reduce_file.py
+++ code/reduce_file.py
@@ -1,9 +1,9 @@
 big=r'/root/data/news_sohusite_xml.dat'
-small=r'/root/data/news_sohusite_xml.100000.dat'
+small=r'../data/news_sohusite_xml.200000.dat'
 flag='</doc>'
-n=100000
+n=200000
 
-fin=open(big)
+fin=open(big,encoding='gb2312',errors='ignore')
 fout=open(small,'w')
 
 for i,line in enumerate(fin):
